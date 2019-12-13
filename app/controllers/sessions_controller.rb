2341cf5e1b33b2dd2842ed5caaf29d2fed1c4484
github username <github email address>
2019-12-12 18:00:11 -0800

    initil commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+    get 'login' do
+        erb :'sessions/login'
+    end
+
+    post '/login' do
+        user = User.find_by(username: params[:username])
+        
+    end
+
+    get '/signup' do
+        erb :'sessions/signup'
+    end
+    
+end
