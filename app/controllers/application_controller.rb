75e12af3f9639510eb60dd1e53b669f5816a4172
github username <github email address>
2019-12-18 18:17:15 -0800

    controllers added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,39 @@ class ApplicationController < Sinatra::Base
     end
 
     get '/' do
-    
         erb :index
+    end
+
+    get '/home' do
+        authenticate
+        erb :home
+    end
+
+    helpers do
+      
+        def logged_in? 
+            !!session[:user_id]
+        end
+
+        def current_user
+            User.find_by(id: session[:user_id])
+        end
+
+        def authenticate
+            if !logged_in?
+                redirect '/login'
+            else
+                @user = current_user
+                @user_routines = current_user.routines
+                @user_products = current_user.products
+            end
+        end
+
+        def authenticate_user(routine)
+            redirect '/home' if !routine
+            redirect '/home' if current_user != routine.user
+        end
+
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_07_010631) do
+ActiveRecord::Schema.define(version: 2019_12_18_023121) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "username"
+    t.string "email"
     t.string "password_digest"
   end
 
